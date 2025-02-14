facab05a34a6a2cb076a66c5adaa09635b6bf4e1
Lavish Meena <67286840+lavishmeena2764@users.noreply.github.com>
2025-02-14 18:56:23 +0530

    feat: update cors url

--- app.js
+++ app.js
@@ -9,7 +9,7 @@ const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cors({
-    origin:'http://localhost:3000'
+    origin:['https://www.globalpathfinders.co.in','https://globalpathfinders.co.in']
 }));
 
 import connDB from './src/config/db.config.js';
